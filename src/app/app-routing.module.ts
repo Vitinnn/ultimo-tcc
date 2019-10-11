d8c7de0ed9005db6854945919a46f149629c3597
Vitinnn <49928840+Vitinnn@users.noreply.github.com>
2019-10-10 22:51:10 -0300

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'receitas', loadChildren: './receitas/receitas.module#ReceitasPageModule' },
+  { path: 'saude', loadChildren: './saude/saude.module#SaudePageModule' },
+  { path: 'entretenimento', loadChildren: './entretenimento/entretenimento.module#EntretenimentoPageModule' },
+  { path: 'tecnologia', loadChildren: './tecnologia/tecnologia.module#TecnologiaPageModule' },  { path: 'limpeza', loadChildren: './limpeza/limpeza.module#LimpezaPageModule' },
+  { path: 'jogos', loadChildren: './jogos/jogos.module#JogosPageModule' },
+  { path: 'noticias', loadChildren: './noticias/noticias.module#NoticiasPageModule' },
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
